7a99d73b768431fb289a940150ff36d119681d3d
Dolawat Wannapira <dolawat.wannapira@gmail.com>
2023-07-05 21:01:35 +0700

    added timestamp to json file request

--- store/modules/laws.js
+++ store/modules/laws.js
@@ -19,8 +19,11 @@ const mutations = {
 
 const actions = {
   async fetchLaws({ commit }) {
+    const timestamp = `${Math.floor(new Date().getTime() / 1000)}${Math.floor(
+      Math.random() * 10000000000
+    )}`;
     const response = await fetch(
-      'https://raw.githubusercontent.com/kaogeek/mfp-promise-tracker/main/data/laws.json'
+      `https://raw.githubusercontent.com/kaogeek/mfp-promise-tracker/main/data/laws.json?t=${timestamp}`
     )
       .then((res) => res.json())
       .then((data) => data)
